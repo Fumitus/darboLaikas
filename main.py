797b5611eca3c87fc0fc389a87077dd304a98154
Andrius Bai <abaikstys@gmail.com>
2025-08-22 16:46:52 +0300

    prideda papildoma diena. Reikia rasti klaidagit add -Agit add -A!

--- main.py
+++ main.py
@@ -3,22 +3,27 @@ from http.cookiejar import month
 from google.oauth2.credentials import Credentials
 from google_auth_oauthlib.flow import InstalledAppFlow
 from googleapiclient.discovery import build
-from datetime import datetime
+from datetime import datetime, timedelta
+from dateutil import parser
 
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
-date_from_console = input("data (YYYY-MM-DD):")
-dt = datetime.strptime(date_from_console, "%Y-%m-%d")
+start_date = input("Periodo pradzia (YYYY-MM-DD):")
+end_date = input("Periodo pabaiga (YYYY-MM-DD):")
+
+dt_start = datetime.strptime(start_date, "%Y-%m-%d")
+dt_end = datetime.strptime(end_date, "%Y-%m-%d")
+
+timeMin = dt_start.isoformat() + 'Z'
+timeMax = dt_end.isoformat() + 'Z'
 
 def main():
     flow = InstalledAppFlow.from_client_secrets_file('client_secret.json', SCOPES)
     creds = flow.run_local_server(port=0)
     service = build('calendar', 'v3', credentials=creds)
 
-    exact_date = dt.isoformat() + 'Z'
-
     events_result = service.events().list(
-        calendarId = 'primary', timeMin=exact_date,
-        maxResults=30, singleEvents=True,
+        calendarId = 'primary', timeMin=timeMin,
+        timeMax=timeMax, maxResults=30, singleEvents=True,
         orderBy = 'startTime'
     ).execute()
 
@@ -29,13 +34,26 @@ def main():
         if e.get('summary', '').startswith(('NV', 'GV'))
     ]
 
+    counts = {'NV': 0, 'GV': 0}
+    durations = {'NV':0, 'GV': 0} #issaugoti valandomis
+
     if not events:
         print('Nera ivykiu')
     for event in filtered_events:
-        start = datetime.fromisoformat(event['start']['dateTime'])
-        end = datetime.fromisoformat(event['end']['dateTime'])
-        duration = end - start
-        print(duration.total_seconds()/3600, event['summary'], event['start']['dateTime'])
+        summary = event.get('summary', '')
+        start = parser.isoparse(event['start']['dateTime'])
+        end = parser.isoparse(event['end']['dateTime'])
+        duration_valandos = (end - start).total_seconds() / 3600
+
+        for prefix in counts.keys():
+            if summary.startswith(prefix):
+                counts[prefix] += 1
+                durations[prefix] += duration_valandos
+
+    print('Darbovietes:', counts)
+    print("isdirbta valandu:", durations)
+
+
 
 if __name__ == '__main__':
     main()
